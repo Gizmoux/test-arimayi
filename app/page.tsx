0b9e85ae0520f0fbc201457895de6c4eb6ad9b5f
Gizmoux <nicolas.deriemacker@hotmail.fr>
2025-02-24 15:58:08 +0100

    ✅ Update Home page with i18n to translate Home Page

--- app/page.tsx
+++ app/page.tsx
@@ -1,19 +1,33 @@
+"use client";
 import { Button } from "antd";
+import { useTranslation } from "next-i18next";
 
 export default function Home() {
+  const { t, i18n } = useTranslation("home");
+  const changeLanguage = (lang: string) => {
+    i18n.changeLanguage(lang);
+    console.log(i18n);
+  };
   return (
     <div>
-      <Button>Home Page</Button>
+      <div>
+        <Button type="primary" className="m-4">
+          {t("accueil")}
+        </Button>
 
-      <Button type="link" href="candidates">
-        Link to Candidates Page
-      </Button>
-      <Button type="link" href="recruiters">
-        Link to Recruiters Page
-      </Button>
-      <Button type="text" className="text-white">
-        Dashed
-      </Button>
+        <Button type="link" href="candidates">
+          {t("candidat")}
+        </Button>
+        <Button type="link" href="recruiters">
+          {t("recruteur")}
+        </Button>
+      </div>
+      <div>
+        <h1>{t("presentation_titre")}</h1>
+        <p>{t("presentation_description")}</p>
+      </div>
+      <Button onClick={() => changeLanguage("fr")}>Français</Button>
+      <Button onClick={() => changeLanguage("en")}>English</Button>
     </div>
   );
 }
